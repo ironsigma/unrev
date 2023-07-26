efd19ec5bd6be335d33ebb86bff88b09f68336bf
Juan Frias <juandfrias@gmail.com>
2023-07-26 16:05:43 -0700

    Better name for temp file during check-out and optional extension

--- unrev-co.sh
+++ unrev-co.sh
@@ -17,12 +17,12 @@ VERSION=1.0.0
 
 
 # Check arg count
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
     cat <<-USAGEMSG
 $SCRIPT v$VERSION - Check-out revision from archive
 
 Usage:
-    $SCRIPT <archive> <rev num>
+    $SCRIPT <archive> <rev num> [.ext]
 
 USAGEMSG
     exit 1
@@ -43,6 +43,9 @@ if [[ "$REVISION_NUM" = "" || $REVISION_NUM -le 0 ]]; then
     exit 1
 fi
 
+# extension suffix to use
+EXT_SUFFIX="${3:-}"
+
 # Get the head revision number
 head_rev=$(7z l -i"!HEAD-R*" "$ARCHIVE_FILE" \
     | grep "HEAD-R[-_.0-9]\+$" \
@@ -54,8 +57,12 @@ if [ $REVISION_NUM -gt $head_rev ]; then
     exit 1
 fi
 
+# create temp file template
+template=$(basename "$ARCHIVE_FILE")
+template="unrev-${template%%.*}-R$REVISION_NUM-XXXXXXXX$EXT_SUFFIX"
+
 # create a temp file and extract the head revision
-base_file=$(mktemp --suffix=,unrev)
+base_file=$(mktemp -t "$template")
 7z e -i"!HEAD-R$head_rev-*" -so "$ARCHIVE_FILE" >> "$base_file"
 
 # apply each remaning revisions until we're done
